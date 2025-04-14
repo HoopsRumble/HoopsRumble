43f4a291c0360179e7ef8d805d54fec81ce95345
HoopsRumble <ahemadfaisal01@gmail.com>
2025-04-14 17:43:34 +0200

    Update submit.js

--- submit.js
+++ submit.js
@@ -2,7 +2,7 @@ document.getElementById("formulaire").addEventListener("submit", function(e) {
   e.preventDefault();
   const form = e.target;
   const data = new FormData(form);
-  fetch("https://SCRIPT_URL", {
+  fetch("https://script.google.com/macros/s/AKfycbzg-y7YNU9OOzrrRwEsEO9O0AItFPfskpFFJLMc-tQrAc4Ro6icXY3FI2xtncB2k-9peA/exec", {
     method: "POST",
     body: data
   }).then(response => {
